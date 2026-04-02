b3e0db91592b2c696c258feb06e1e26352b9666d
Karol Mierzwinski <karol.mierzwinski2@gmail.com>
2026-04-02 20:01:26 +0200

    Sceleton2

--- backend/CtfPlatformLocal/app/urls.py
+++ backend/CtfPlatformLocal/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('todos/', views.todos, name='todos'),
+    path('login', views.login, name='login'),
 ]
--- backend/CtfPlatformLocal/app/views.py
+++ backend/CtfPlatformLocal/app/views.py
@@ -1,9 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from .models import ToDoItem
 
 # Create your views here.
 def home(request):
     return render(request, 'homePage.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username == 'admin' and password == 'admin':
+            return JsonResponse({"message": "Login successful!"})
+        else:
+            return JsonResponse({"message": "Invalid credentials."})
+    return JsonResponse({"message": "Only POST allowed."})
 
 def todos(request):
     items = ToDoItem.objects.all()
