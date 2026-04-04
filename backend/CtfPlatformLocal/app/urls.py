8ad0d5c2b945d6bf9a5a041c221adba565dfaa82
Karol Mierzwinski <karol.mierzwinski2@gmail.com>
2026-04-04 13:24:53 +0200

    Login/Register

--- backend/CtfPlatformLocal/app/urls.py
+++ backend/CtfPlatformLocal/app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('todos/', views.todos, name='todos'),
     path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('mainhub/', views.mainhub, name='mainhub'),
 ]
--- backend/CtfPlatformLocal/app/views.py
+++ backend/CtfPlatformLocal/app/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from .models import ToDoItem
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
 def home(request):
     return render(request, 'homePage.html')
 
@@ -10,11 +12,34 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        if username == 'admin' and password == 'admin':
-            return JsonResponse({"message": "Login successful!"})
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return JsonResponse({"redirect": "/mainhub", "message": "Login successful!"})
         else:
             return JsonResponse({"message": "Invalid credentials."})
-    return JsonResponse({"message": "Only POST allowed."})
+    return JsonResponse({"message": "Please fill in the form."})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"message": "User already exists"})
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        return JsonResponse({"message": "User created successfully"})
+
+@login_required
+def mainhub(request):
+    return render(request, 'mainhub.html')
 
 def todos(request):
     items = ToDoItem.objects.all()
